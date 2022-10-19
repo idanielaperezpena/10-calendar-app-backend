dab1c5a5164da41bb030d1941b7f3d2fcc519185
Daniela Perez <idanielaperezpena@gmail.com>
2022-10-19 11:47:39 -0500

    added public folder and env variables

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+require('dotenv').config()
 
 //server
 const app = express()
 
+//public folder
+app.use(express.static('public'))
+
+//endPoints
+// app.get('/', (req, res) => {
+//     res.json({
+//         ok: true
+//     })
+// })
+
 //listen
-app.listen(4000, () => {
-    console.log(`Servidor corriendo en puerto ${4000}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })
