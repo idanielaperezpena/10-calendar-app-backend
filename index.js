c25dd3fb38bd9610bf520defeb68238455249dd0
Daniela Perez <idanielaperezpena@gmail.com>
2022-10-19 16:14:19 -0500

    added database connection

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const { dbConnection } = require('./database/config')
 require('dotenv').config()
 
 //server
 const app = express()
+
+//Database
+dbConnection()
 
 //public folder
 app.use(express.static('public'))
