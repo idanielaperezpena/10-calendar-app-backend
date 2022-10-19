6ef24f9277f6af2ac7993abfa3bd48e971cdbee1
Daniela Perez <idanielaperezpena@gmail.com>
2022-10-19 17:10:39 -0500

    added login service with JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,39 +1,82 @@
 const { response } = require('express')
 const bcrypt = require('bcryptjs')
+const { generateJWT } = require('../helpers/jwt')
 const User = require('../models/User')
 
 const newUser = async (req, res = response) => {
     const { email, password } = req.body
+    try {
+        let user = await User.findOne({ email })
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                message: 'This email has already been used',
+            })
+        }
+        user = new User(req.body)
+        //encrypt password
+        const salt = bcrypt.genSaltSync()
+        user.password = bcrypt.hashSync(password, salt)
 
-    let user = await User.findOne({ email })
-    if (user) {
-        return res.status(400).json({
+        await user.save()
+
+        const token = await generateJWT(user.id, user.name)
+        res.status(200).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
             ok: false,
-            message: 'This email has already been used',
+            message: 'Contact the system administrator',
+
         })
     }
-    user = new User(req.body)
-    //encrypt password
-    const salt = bcrypt.genSaltSync()
-    user.password = bcrypt.hashSync(password, salt)
-    
-    await user.save()
 
-    res.status(201).json({
-        ok: true,
-        message: 'register',
-        uid: user.id,
-        name: user.name
-    })
+
 }
 
-const userLogin = (req, res = response) => {
+const userLogin = async (req, res = response) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                message: 'User not found',
+            })
+        }
 
-    res.status(200).json({
-        ok: true,
-        message: 'login',
-        user: req.body
-    })
+        //check passwords
+        const validPassword = bcrypt.compareSync(password, user.password)
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Invalid password',
+            })
+        }
+
+        //JWT
+        const token = await generateJWT(user.id, user.name)
+        res.status(200).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            message: 'Contact the system administrator',
+
+        })
+    }
+
 }
 
 const tokenRenew = (req, res = response) => {
